f63f71d034bbd93034711fde5c58bd938ff54b46
Brennan Fee <brennan.git@mailbots.org>
2024-08-21 15:30:13 -0500

    fix: Fixed terminal color mappings

--- lua/oncedark/highlights/terminal.lua
+++ lua/oncedark/highlights/terminal.lua
@@ -10,23 +10,23 @@ function M.groups(theme)
     terminal_color_0 = theme.palette.black,
     terminal_color_8 = theme.palette.gray,
 
-    terminal_color_1 = theme.palette.bright_red,
-    terminal_color_9 = color.brighten(theme.palette.bright_red, 15),
+    terminal_color_1 = theme.palette.red,
+    terminal_color_9 = theme.palette.bright_red,
 
-    terminal_color_2 = theme.palette.bright_green,
-    terminal_color_10 = color.brighten(theme.palette.bright_green, 15),
+    terminal_color_2 = theme.palette.green,
+    terminal_color_10 = theme.palette.bright_green,
 
     terminal_color_3 = theme.palette.yellow,
-    terminal_color_11 = color.brighten(theme.palette.yellow, 15),
+    terminal_color_11 = theme.palette.bright_yellow,
 
     terminal_color_4 = theme.palette.blue,
-    terminal_color_12 = color.brighten(theme.palette.blue, 15),
+    terminal_color_12 = theme.palette.bright_blue,
 
     terminal_color_5 = theme.palette.magenta,
-    terminal_color_13 = color.brighten(theme.palette.magenta, 15),
+    terminal_color_13 = theme.palette.bright_magenta,
 
     terminal_color_6 = theme.palette.cyan,
-    terminal_color_14 = color.brighten(theme.palette.cyan, 15),
+    terminal_color_14 = theme.palette.bright_cyan,
 
     terminal_color_7 = theme.palette.fg,
     terminal_color_15 = theme.palette.fg,
